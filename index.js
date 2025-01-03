79d976e1b4a5b015f5806b0eab697efb25672c2c
louis-ngugi <louisngugi858@gmail.com>
2025-01-03 15:04:58 +0300

    update javascript-functions-continued

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-// code your solution here
+// Function declaration for saturdayFun
 function saturdayFun(activity = "roller-skate") {
-    return `This Saturday, I want to ${activity}!`;
-  }
-  
-  saturdayFun(); 
+  return `This Saturday, I want to ${activity}!`;
+}
 
-saturdayFun("bathe my dog!");
+// Call examples
+console.log(saturdayFun()); // Default: "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("bathe my dog")); // Custom: "This Saturday, I want to bathe my dog!"
 
+// Function expression for mondayWork
 const mondayWork = function(activity = "go to the office") {
-  return(`This Monday, I will ${activity}.`);
+  return `This Monday, I will ${activity}.`;
 };
 
-mondayWork();
-mondayWork("work from home");
+// Call examples
+console.log(mondayWork()); // Default: "This Monday, I will go to the office."
+console.log(mondayWork("work from home")); // Custom: "This Monday, I will work from home."
 
+// Function for wrapAdjective
 function wrapAdjective(wrapper = "*") {
   return function(adjective = "special") {
     return `You are ${wrapper}${adjective}${wrapper}!`;
   };
 }
 
-
+// Call examples
 let result = wrapAdjective('*');
-console.log(result("a hard worker")); 
+console.log(result("a hard worker")); // "You are *a hard worker*!"
 
 result = wrapAdjective('||');
-console.log(result("a dedicated programmer")); 
+console.log(result("a dedicated programmer")); // "You are ||a dedicated programmer||!"
